512d345654f8a12a52402f848c5e947de0c9553a
Giovanni Pires da Silva <giovannipds@gmail.com>
2017-07-07 14:05:58 -0300

    Activate selectedHero's highlight through class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li [class.selected]="hero === selectedHero" (click)="onSelect(hero)" *ngFor="let hero of heroes">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
@@ -36,7 +36,7 @@ const HEROES: Hero[] = [
         <input [(ngModel)]="selectedHero.name" placeholder="name">
       </div>
     </div>
-    `,
+  `,
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -91,6 +91,7 @@ export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes: Hero[] = HEROES;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
