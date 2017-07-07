1755792e847adea56b5baee9820656d5ab2bd774
Giovanni Pires da Silva <giovannipds@gmail.com>
2017-07-07 15:06:41 -0300

    Separate the mock data and constant of heroes (and export it) into HeroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'app-root',
   template: `
@@ -80,7 +67,7 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
