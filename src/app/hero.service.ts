bedff2e40e5a4545970dd339a124c9f8d1b6ee7d
Giovanni Pires da Silva <giovannipds@gmail.com>
2017-07-10 11:00:59 -0300

    Getting heroes from the hero web api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
+
+  constructor(private http: Http) { }
+
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
       .then(heroes => heroes.find(hero => hero.id === id));
   }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error ocurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
       // Simulate server latency with 2 second delay
