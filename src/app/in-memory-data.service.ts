90ae676b24dc19b512a93ad1ce00b9cf62dd7fdb
Giovanni Pires da Silva <giovannipds@gmail.com>
2017-07-10 14:20:17 -0300

    Put the game of thrones names in in memory data =P

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,17 +2,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 0,  name: 'Zero' },
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 0, name: 'Tyrion Lannister' },
+      { id: 1, name: 'Jaime Lannister' },
+      { id: 2, name: 'Cersei Lannister' },
+      { id: 3, name: 'Daenerys Targaryen' },
+      { id: 4, name: 'Jon Snow' },
+      { id: 5, name: 'Petyr Baelish' },
+      { id: 6, name: 'Jorah Mormont' },
+      { id: 7, name: 'Sansa Stark' },
+      { id: 8, name: 'Arya Stark' },
+      { id: 9, name: 'Theon Greyjoy' }
     ];
     return {heroes};
   }
