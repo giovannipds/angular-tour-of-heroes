c9a907c8e953628c87e08c237ca520c480e5e900
Giovanni Pires da Silva <giovannipds@gmail.com>
2017-07-10 08:39:42 -0300

    Configure the RouterModule and check if there's a base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -22,3 +23,10 @@ import { HeroService } from './hero.service';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+])
